fcd8152df84dbbdc7ac80630f63a06742246362d
Juan Pablo Ibañez <ing.juanpablo@gmail.com>
2020-08-13 18:21:27 -0300

    Initial Commit

---
+++ NotificationHubSample/NotificationHubSample.iOS/AppDelegate.cs
@@ -0,0 +1,137 @@
+﻿using Foundation;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using UIKit;
+using UserNotifications;
+using WindowsAzure.Messaging;
+
+namespace NotificationHubSample.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        private SBNotificationHub Hub { get; set; }
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            base.FinishedLaunching(app, options);
+
+            RegisterForRemoteNotifications();
+
+            return true;
+        }
+
+        void RegisterForRemoteNotifications()
+        {
+            // register for remote notifications based on system version
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert |
+                    UNAuthorizationOptions.Sound |
+                    UNAuthorizationOptions.Sound,
+                    (granted, error) =>
+                    {
+                        if (granted)
+                            InvokeOnMainThread(UIApplication.SharedApplication.RegisterForRemoteNotifications);
+                    });
+            }
+            else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+                UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                new NSSet());
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications();
+            }
+            else
+            {
+                UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+                UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
+            }
+        }
+
+        private string ExtractToken(NSData deviceToken)
+        {
+            if (deviceToken.Length == 0)
+                return null;
+            var result = new byte[deviceToken.Length];
+            System.Runtime.InteropServices.Marshal.Copy(deviceToken.Bytes, result, 0, (int)deviceToken.Length);
+            return BitConverter.ToString(result).Replace("-", "");
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            Hub = new SBNotificationHub(AppConstants.ListenConnectionString, AppConstants.NotificationHubName);
+
+            var token = ExtractToken(deviceToken);
+
+            // update registration with Azure Notification Hub
+            Hub.UnregisterAll(token, (error) =>
+            {
+                if (error != null)
+                {
+                    Debug.WriteLine($"Unable to call unregister {error}");
+                    return;
+                }
+
+                var tags = new NSSet(AppConstants.SubscriptionTags.ToArray());
+                Hub.RegisterNative(token, tags, (errorCallback) =>
+                {
+                    if (errorCallback != null)
+                    {
+                        Debug.WriteLine($"RegisterNativeAsync error: {errorCallback}");
+                    }
+                });
+
+                var templateExpiration = DateTime.Now.AddDays(120).ToString(System.Globalization.CultureInfo.CreateSpecificCulture("en-US"));
+                Hub.RegisterTemplate(token, "defaultTemplate", AppConstants.APNTemplateBody, templateExpiration, tags, (errorCallback) =>
+                {
+                    if (errorCallback != null)
+                    {
+                        if (errorCallback != null)
+                        {
+                            Debug.WriteLine($"RegisterTemplateAsync error: {errorCallback}");
+                        }
+                    }
+                });
+            });
+        }
+
+        public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+        {
+            ProcessNotification(userInfo, false);
+        }
+
+        void ProcessNotification(NSDictionary options, bool fromFinishedLaunching)
+        {
+            // make sure we have a payload
+            if (options != null && options.ContainsKey(new NSString("aps")))
+            {
+                // get the APS dictionary and extract message payload. Message JSON will be converted
+                // into a NSDictionary so more complex payloads may require more processing
+                NSDictionary aps = options.ObjectForKey(new NSString("aps")) as NSDictionary;
+                string payload = string.Empty;
+                NSString payloadKey = new NSString("alert");
+                if (aps.ContainsKey(payloadKey))
+                {
+                    payload = aps[payloadKey].ToString();
+                }
+
+                if (!string.IsNullOrWhiteSpace(payload))
+                {
+                    (App.Current.MainPage as MainPage)?.AddMessage(payload);
+                }
+
+            }
+            else
+            {
+                Debug.WriteLine($"Received request to process notification but there was no payload.");
+            }
+        }
+    }
+}
